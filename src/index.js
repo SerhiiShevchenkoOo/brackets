d424a46f933b1186ef9cab3315e0b113984f7a8d
Serhii Shevchenko <serhiii.shevchenko@gmail.com>
2020-10-11 23:07:53 +0300

    fear: complete task..3days!!!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // let  brackets = bracketsConfig.map(arrays=> arrays.join('')).join('')
+    let brackets = new Map()
+    let stack = []
+    bracketsConfig
+        .forEach(arrays => {
+            brackets
+                .set(arrays[0], arrays[1]);
+        });
+    // iteration?
+
+    for (let i = 0; i < str.length; i++) {
+        (str[i] === stack[stack.length - 1]) ?
+            stack.pop() :
+            stack.push(brackets.get(str[i]));
+    }
+    return stack.length === 0
 }
+
